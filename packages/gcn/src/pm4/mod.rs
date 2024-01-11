bfbd657095e8587b8de7adcf5107ac57e5e32476
martin <martincharles07@gmail.com>
2024-01-10 22:22:06 -0600

    lift packet parsing
    coming up with a structure i can leave chatgpt to write

--- packages/gcn/src/pm4/mod.rs
+++ packages/gcn/src/pm4/mod.rs
@@ -91,10 +91,46 @@ pub enum Type3PacketValue {
     SetShaderRegister {
         values: Vec<ShaderRegisterSetOperation>,
     },
+    EndOfPipe(EndOfPipePacket),
     Unknown {
         opcode: OpCode,
         body: Vec<u32>,
     },
+}
+
+#[derive(Debug)]
+pub struct EndOfPipePacket {
+    pub invalidate_writeback_l2: bool,
+    pub event_index: u8,
+    pub event_type: u8,
+    pub address: u64,
+    pub immediate: u64,
+}
+
+impl EndOfPipePacket {
+    pub fn from_type3_packet(body: Vec<u32>) -> EndOfPipePacket {
+        let event_control = body[0];
+
+        let inv_l2 = if bitrange(20, 20).of_32(event_control) == 1 {
+            true
+        } else {
+            false
+        };
+        let event_index = bitrange(11, 8).of_32(event_control) as u8;
+        let event_type = bitrange(5, 0).of_32(event_control) as u8;
+
+        let address = (body[1] as u64) | ((bitrange(15, 0).of_32(body[2]) << 32) as u64);
+        let immediate = (body[3] as u64) | ((body[4] as u64) << 32);
+
+        EndOfPipePacket {
+            invalidate_writeback_l2: inv_l2,
+            event_index,
+            event_type,
+
+            address,
+            immediate,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -149,6 +185,9 @@ impl Type3PacketValue {
                         })
                         .collect(),
                 }
+            }
+            OpCode::EVENT_WRITE_EOP => {
+                Type3PacketValue::EndOfPipe(EndOfPipePacket::from_type3_packet(body))
             }
             _ => Type3PacketValue::Unknown { opcode, body },
         }
