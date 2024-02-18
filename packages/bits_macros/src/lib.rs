7f0171f9de577b2f4a74717386468098d877c4c4
martin <martincharles07@gmail.com>
2024-02-18 16:56:36 -0600

    add docs

--- packages/bits_macros/src/lib.rs
+++ packages/bits_macros/src/lib.rs
@@ -4,6 +4,26 @@ use crate::derive_from_bits::derive_from_bits;
 use proc_macro::TokenStream;
 use syn::DeriveInput;
 
+/// # Example
+///
+/// ```
+/// use bits_macros::FromBits;
+///
+/// #[derive(FromBits)]
+/// #[bits(64)]
+/// struct Item {
+///     #[bits(31, 0)]
+///     field1: u32,
+///
+///     #[bits(40, 32)]
+///     field2: u8,
+/// }
+/// ```
+///
+/// The struct attribute `#[bits(N)]` specifies the BITS const argument of the `FromBits` trait.
+/// The field attributes `#[bits(highest_bit, lowest_bit)]` specifies the inclusive range of bits
+/// which correspond to the annotated field. Bit 0 is interpreted as the least significant bit and
+/// bit N, the most significant bit. Both bounds are inclusive.
 #[proc_macro_derive(FromBits, attributes(bits))]
 pub fn derive_from_bits_exported(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
