fb5a4c75b70f06b763243464bbf36b523777acd6
martin <martincharles07@gmail.com>
2024-02-22 15:35:45 -0600

    log to memory
    this is the write half of the flush queue, now we need to read from the queue
    
    next, write the consumer. going to poll for now as I don't want to deal with the complexity of condvars

--- goldhen_plugins/plugin_src/extern_traces/source/main.c
+++ goldhen_plugins/plugin_src/extern_traces/source/main.c
@@ -71,12 +71,100 @@ int lazy_file()
     return fd;
 }
 
-void extern_logf(const char *str)
+#define HIGH_WATER_MARK_BYTES (16 * 1024)
+#define MAX_BUFFERED_BYTES (64 * 1024)
+
+_Static_assert(
+    HIGH_WATER_MARK_BYTES < MAX_BUFFERED_BYTES,
+    "high water mark must be smaller than max buffer size"
+);
+
+// Not sure of the exact size of the mutex, so blocking off more bytes than we
+// probably need.
+typedef char ScePthreadMutex[1024];
+
+#define MAX_FLUSH_QUEUE_ENTRIES 1024
+
+typedef struct FlushQueueEntry {
+    char buffer[MAX_BUFFERED_BYTES];
+    uint32_t buffer_len;
+} FlushQueueEntry;
+
+typedef struct FlushQueue {
+    FlushQueueEntry entries[MAX_FLUSH_QUEUE_ENTRIES];
+    uint32_t read_head_idx;
+    uint32_t write_head_idx;
+} FlushQueue;
+
+static FlushQueue flush_queue;
+static ScePthreadMutex flush_queue_mutex;
+
+typedef struct ThreadLoggingState {
+    char buffer[MAX_BUFFERED_BYTES];
+    uint32_t buffer_idx;
+    uint64_t start_time;
+} ThreadLoggingState;
+
+void flush_queue_add(ThreadLoggingState* input, FlushQueue* queue) {
+    uint32_t write_head = queue->write_head_idx;
+
+    if (write_head >= MAX_FLUSH_QUEUE_ENTRIES) {
+        final_printf("flush queue full, no more space, dropping logs\n");
+        return;
+    }
+
+    // add to flush queue
+    FlushQueueEntry* entry = &queue->entries[write_head];
+    strncpy(entry->buffer, input->buffer, input->buffer_idx);
+    entry->buffer_len = input->buffer_idx;
+
+    uint32_t next_write_head = write_head + 1;
+    queue->write_head_idx = next_write_head;
+
+    // reset input
+    input->buffer_idx = 0;
+    input->start_time = 0;
+}
+
+__thread ThreadLoggingState logging_state;
+
+int sceRtcGetCurrentTick(uint64_t* tick);
+int scePthreadMutexTrylock(ScePthreadMutex* mutex);
+int scePthreadMutexUnlock(ScePthreadMutex* mutex);
+
+void extern_logf(const char *msg)
 {
-    int fd = lazy_file();
-    int len = strlen(str);
-    sceKernelWrite(fd, str, len);
-    sceKernelFsync(fd);
+    int len = strlen(msg);
+    strncpy(&logging_state.buffer[logging_state.buffer_idx], msg, len);
+    logging_state.buffer_idx += len;
+
+    uint64_t current_time;
+    int ret = sceRtcGetCurrentTick(&current_time);
+    if (ret != 0) {
+        final_printf("sceRtcGetCurrentTick failed\n");
+        return;
+    }
+
+    if (!logging_state.start_time) {
+        logging_state.start_time = current_time;
+    }
+
+    if (logging_state.buffer_idx >= HIGH_WATER_MARK_BYTES || (current_time - logging_state.start_time) >= 1 * 1000 * 1000) {
+        // if past the high water mark or more than 1 second since the buffer init, attempt to flush
+        int ret = scePthreadMutexTrylock(&flush_queue_mutex);
+        if (!ret) {
+            // locked, add to flush queue
+            flush_queue_add(&logging_state, &flush_queue);
+            int ret = scePthreadMutexUnlock(&flush_queue_mutex);
+            if (ret) {
+                // error occured
+                final_printf("scePThreadMutexUnlock non-ok value %x\n", ret);
+            }
+        } else {
+            // failed to acquire lock, in this case wait for future logs
+            // invocations to handle this
+        }
+    }
 }
 
 #pragma GCC diagnostic push
