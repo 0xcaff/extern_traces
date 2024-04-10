507a660140829fee0c0c059b57bcba91528f571f
martin <martincharles07@gmail.com>
2024-04-10 01:06:07 -0400

    add a bunch of test data

--- packages/pm4/src/packet.rs
+++ packages/pm4/src/packet.rs
@@ -114,4 +114,13 @@ mod tests {
         insta::assert_debug_snapshot!(packets);
         insta::assert_debug_snapshot!(convert(packets.as_slice()));
     }
+
+    #[test]
+    fn sample1() {
+        let bytes = &include_bytes!("./test_data/354185f086261349c2311cb16f78cd2d23694b585827d5d21497810e759a1b40.draw.pm4")[..];
+        let packets = PM4Packet::parse_all(bytes).unwrap();
+
+        insta::assert_debug_snapshot!(packets);
+        insta::assert_debug_snapshot!(convert(packets.as_slice()));
+    }
 }
