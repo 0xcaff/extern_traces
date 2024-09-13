80e7314d7164acd7756ab0c941014034298051e3
martin <martincharles07@gmail.com>
2024-09-13 13:36:03 -0500

    strip down extern_traces
    we're making headway here. hoping this test succeeds excited to have thread local storage to implement a rock solid logger

--- goldhen_plugins/plugin_src/extern_traces/source/main.c
+++ goldhen_plugins/plugin_src/extern_traces/source/main.c
@@ -2,31 +2,25 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#include "logging.h"
+#include "thread_local_storage.h"
 #include "plugin_common.h"
 
 attr_public const char *g_pluginName = "extern_traces";
-attr_public const char *g_pluginDesc = "Collects traces for external calls";
+attr_public const char *g_pluginDesc = "collects traces for external calls";
 attr_public const char *g_pluginAuth = "0xcaff";
 attr_public uint32_t g_pluginVersion = 0x00000100; // 1.00
-
-#include "trampolines.h"
 
 s32 attr_module_hidden module_start(s64 argc, const void *args)
 {
     final_printf("[GoldHEN] %s Plugin Started.\n", g_pluginName);
     final_printf("[GoldHEN] <%s\\Ver.0x%08x> %s\n", g_pluginName, g_pluginVersion, __func__);
     final_printf("[GoldHEN] Plugin Author(s): %s\n", g_pluginAuth);
-
-    register_hooks();
-
-    OrbisPthread thread;
-    int ret = scePthreadCreate(&thread, NULL, flush_thread_start_routine, NULL, STRINGIFY(flush_thread_start_routine));
-    if (ret)
-    {
-        final_printf("scePthreadCreate failed %x\n", ret);
-    }
+    final_printf("[GoldHEN] validate tls: %s\n", validate_fs_register() ? "true" : "false");
+    final_printf("[GoldHEN] read tls value first: %lx\n", read_tls_value());
+    write_tls_value(0xdeadbeef);
+    final_printf("[GoldHEN] read tls value second: %lx\n", read_tls_value());
 
     return 0;
 }
@@ -35,6 +29,6 @@ s32 attr_module_hidden module_stop(s64 argc, const void *args)
 {
     final_printf("[GoldHEN] <%s\\Ver.0x%08x> %s\n", g_pluginName, g_pluginVersion, __func__);
     final_printf("[GoldHEN] %s Plugin Ended.\n", g_pluginName);
-    UNHOOK(sceAudioOutInit);
+
     return 0;
 }
