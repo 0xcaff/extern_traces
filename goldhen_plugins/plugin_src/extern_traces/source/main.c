627f064f1d5bbe9109d06d63592b461844bdba13
martin <martincharles07@gmail.com>
2024-09-13 23:22:32 -0500

    add flush thread

--- goldhen_plugins/plugin_src/extern_traces/source/main.c
+++ goldhen_plugins/plugin_src/extern_traces/source/main.c
@@ -12,15 +12,22 @@ attr_public const char *g_pluginDesc = "collects traces for external calls";
 attr_public const char *g_pluginAuth = "0xcaff";
 attr_public uint32_t g_pluginVersion = 0x00000100; // 1.00
 
+void* flush_thread(void* arg) {
+    return NULL;
+}
+
 s32 attr_module_hidden module_start(s64 argc, const void *args)
 {
     final_printf("[GoldHEN] %s Plugin Started.\n", g_pluginName);
     final_printf("[GoldHEN] <%s\\Ver.0x%08x> %s\n", g_pluginName, g_pluginVersion, __func__);
     final_printf("[GoldHEN] Plugin Author(s): %s\n", g_pluginAuth);
-    final_printf("[GoldHEN] validate tls: %s\n", validate_fs_register() ? "true" : "false");
-    final_printf("[GoldHEN] read tls value first: %lx\n", read_tls_value());
-    write_tls_value(0xdeadbeef);
-    final_printf("[GoldHEN] read tls value second: %lx\n", read_tls_value());
+
+    OrbisPthread thread;
+    int ret = scePthreadCreate(&thread, NULL, flush_thread, NULL, STRINGIFY(flush_thread_start_routine));
+    if (ret)
+    {
+        final_printf("[extern_traces] scePthreadCreate failed %x\n", ret);
+    }
 
     return 0;
 }
