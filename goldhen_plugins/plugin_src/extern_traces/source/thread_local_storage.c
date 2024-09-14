a768e0f979f1af46a94b69b9106e3e867b069479
martin <martincharles07@gmail.com>
2024-09-14 16:57:42 -0500

    remove unused

--- goldhen_plugins/plugin_src/extern_traces/source/thread_local_storage.c
+++ goldhen_plugins/plugin_src/extern_traces/source/thread_local_storage.c
@@ -21,27 +21,3 @@ void write_tls_value(uint64_t new_value) {
         : "memory"
     );
 }
-
-bool validate_fs_register() {
-    uint64_t fs_value;
-    uint64_t fs0_value;
-
-    // Read the value at fs:0 into fs0_value
-    __asm__ volatile (
-        "movq %%fs:0, %0;"      // Read the QWORD at [fs:0] into fs0_value
-        : "=r" (fs0_value)      // Output register for fs0_value
-        :                       // No input registers
-        : "memory"
-    );
-
-    // Read the value of the fs segment register into fs_value
-    __asm__ volatile (
-        "movq %%fs, %0;"        // Read the fs segment base address into fs_value
-        : "=r" (fs_value)       // Output register for fs_value
-        :                       // No input registers
-        : "memory"
-    );
-
-    // Assert that the values are equal
-    return fs_value == fs0_value;
-}
--- goldhen_plugins/plugin_src/extern_traces/source/thread_local_storage.h
+++ goldhen_plugins/plugin_src/extern_traces/source/thread_local_storage.h
@@ -3,4 +3,3 @@
 
 uint64_t read_tls_value();
 void write_tls_value(uint64_t new_value);
-bool validate_fs_register();
