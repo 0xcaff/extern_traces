1ed028502fbac1c13affbf418c6eb2d815b56299
martin <martincharles07@gmail.com>
2024-09-28 20:35:21 -0500

    add shader collecting
    now need to dump them into the buffer somehow
    
    i'm beginning to think trading off interop is a good idea.

---
+++ packages/plugin_support/src/ffi.rs
@@ -0,0 +1,75 @@
+#[repr(C)]
+pub struct Args {
+    pub xmm0: [u64; 2],
+    pub xmm1: [u64; 2],
+    pub xmm2: [u64; 2],
+    pub xmm3: [u64; 2],
+    pub xmm4: [u64; 2],
+    pub xmm5: [u64; 2],
+    pub xmm6: [u64; 2],
+    pub xmm7: [u64; 2],
+
+    pub args: [u64; 6],
+    extra_args: [u64; 0],
+}
+
+impl Args {
+    pub unsafe fn extra_args_mut_ptr(&mut self, arg_idx: usize) -> *mut u64 {
+        self.extra_args.as_mut_ptr().offset(arg_idx as _)
+    }
+
+    pub unsafe fn extra_args_ptr(&self, arg_idx: usize) -> *const u64 {
+        self.extra_args.as_ptr().offset(arg_idx as _)
+    }
+}
+
+#[repr(C)]
+pub struct BufferReservation {
+    buffer: *mut (),
+    write_idx: u64,
+    available_bytes: u64,
+    is_new: bool,
+}
+
+#[repr(C)]
+pub struct ThreadLoggingState {
+    _private: [u8; 0],
+}
+
+extern "C" {
+    fn thread_logging_state_reserve_space(
+        thread_state: *mut ThreadLoggingState,
+        length: usize,
+    ) -> BufferReservation;
+
+    fn buffer_reservation_write(
+        reservation: *mut BufferReservation,
+        data: *const libc::c_uchar,
+        length: usize,
+    );
+
+    fn thread_logging_state_flush_reservation(
+        thread_state: *mut ThreadLoggingState,
+        reservation: BufferReservation,
+    );
+}
+
+impl ThreadLoggingState {
+    pub fn reserve(&mut self, length: usize) -> BufferReservation {
+        unsafe { thread_logging_state_reserve_space(self, length) }
+    }
+
+    pub fn flush(&mut self, buffer_reservation: BufferReservation) {
+        unsafe {
+            thread_logging_state_flush_reservation(self, buffer_reservation);
+        }
+    }
+}
+
+impl BufferReservation {
+    pub fn write(&mut self, data: &[u8]) {
+        unsafe {
+            buffer_reservation_write(self, data.as_ptr(), data.len());
+        }
+    }
+}
--- packages/plugin_support/src/lib.rs
+++ packages/plugin_support/src/lib.rs
@@ -1,9 +1,15 @@
 #![no_std]
 #![feature(lang_items)]
 #![feature(core_intrinsics)]
+#![feature(ptr_as_ref_unchecked)]
+extern crate alloc;
 
+mod ffi;
 mod io;
 
+use alloc::collections::btree_map::Entry;
+use crate::ffi::{Args, ThreadLoggingState};
+use alloc::collections::BTreeMap;
 use core::alloc::{GlobalAlloc, Layout};
 use core::panic::PanicInfo;
 use core::slice;
@@ -43,55 +49,117 @@ unsafe impl GlobalAlloc for MyAllocator {
 #[global_allocator]
 static GLOBAL: MyAllocator = MyAllocator;
 
+unsafe fn command_buffers<'a>(
+    count: usize,
+    addrs: *const *const u8,
+    sizes: *const u32,
+) -> impl Iterator<Item = &'a [u8]> {
+    let draw_command_buffer_addrs = slice::from_raw_parts(addrs, count);
+    let draw_command_buffer_sizes = slice::from_raw_parts(sizes, count);
+
+    (0usize..count).into_iter().map(|idx| {
+        slice::from_raw_parts(
+            draw_command_buffer_addrs[idx],
+            draw_command_buffer_sizes[idx] as usize,
+        )
+    })
+}
+
 #[no_mangle]
-extern "C" fn log_pm4(buffer: *const u8, len: usize) {
-    let buffer = unsafe { slice::from_raw_parts(buffer, len) };
+extern "C" fn sceGnmSubmitAndFlipCommandBuffersForWorkload_trace(
+    args: *const Args,
+    thread_logging_state: *mut ThreadLoggingState,
+) {
+    let args = unsafe { args.as_ref_unchecked() };
+    let thread_logging_state = unsafe { thread_logging_state.as_mut_unchecked() };
 
-    let buffer = PM4Packet::parse_all(buffer).unwrap();
-    let (commands, _ignored_packets, _ignored_registers) = convert(&buffer).unwrap();
-    for command in commands {
-        match command {
-            Command::Draw { pipeline, .. } => {
-                if let Some(vertex_shader) = pipeline.vertex_shader.entrypoint_gpu_address {
-                    if vertex_shader != 0 {
-                        let invocation =
-                            unsafe { ShaderInvocation::decode_from_memory(vertex_shader) }.unwrap();
+    let count = args.args[1] as u32;
+    let draw_buffers = args.args[2] as *const *const u8;
+    let draw_sizes = args.args[3] as *const u32;
+    let compute_buffers = args.args[4] as *const *const u8;
+    let compute_sizes = args.args[5] as *const u32;
 
-                        println!(
-                            "vertex @ 0x{:x} len {}",
-                            (vertex_shader as u64) << 8,
-                            invocation.bytes.len() * 4
-                        );
-                    }
-                }
+    let mut shaders = ShadersCollector::new();
+    
+    let draw_command_buffers = unsafe { command_buffers(count as _, draw_buffers, draw_sizes) };
+    for command_buffer in draw_command_buffers {
+        shaders.collect(command_buffer);
+    }
+    
+    let compute_command_buffers =
+        unsafe { command_buffers(count as _, compute_buffers, compute_sizes) };
+    for command_buffer in compute_command_buffers {
+        shaders.collect(command_buffer);
+    }
+    
+    
+}
 
-                if let Some(pixel_shader) = pipeline.pixel_shader.address {
-                    if pixel_shader != 0 {
-                        let invocation =
-                            unsafe { ShaderInvocation::decode_from_memory(pixel_shader) }.unwrap();
+enum ShaderKind {
+    Compute,
+    Vertex,
+    Pixel,
+}
 
-                        println!(
-                            "pixel @ 0x{:x} len {}",
-                            (pixel_shader as u64) << 8,
-                            invocation.bytes.len() * 4
-                        );
-                    }
-                }
-            }
-            Command::Dispatch { pipeline, .. } => {
-                if pipeline.address_lo != 0 {
-                    let invocation =
-                        unsafe { ShaderInvocation::decode_from_memory(pipeline.address_lo) }
-                            .unwrap();
+struct Shader {
+    kind: ShaderKind,
+    shader_invocation: ShaderInvocation,
+}
 
-                    println!(
-                        "compute @ 0x{:x} len {}",
-                        (pipeline.address_lo as u64) << 8,
-                        invocation.bytes.len() * 4
-                    );
-                }
-            }
-            _ => {}
+struct ShadersCollector {
+    shaders: BTreeMap<u32, Shader>,
+}
+
+impl ShadersCollector {
+    pub fn new() -> ShadersCollector {
+        ShadersCollector {
+            shaders: BTreeMap::new()
         }
+    }
+}
+
+impl ShadersCollector {
+    pub fn collect(&mut self, buffer: &[u8]) {
+        let buffer = PM4Packet::parse_all(buffer).unwrap();
+        let (commands, _ignored_packets, _ignored_registers) = convert(&buffer).unwrap();
+        for command in commands {
+            match command {
+                Command::Draw { pipeline, .. } => {
+                    if let Some(vertex_shader) = pipeline.vertex_shader.entrypoint_gpu_address {
+                        self.collect_shader(vertex_shader, ShaderKind::Vertex);
+                    }
+
+                    if let Some(pixel_shader) = pipeline.pixel_shader.address {
+                        self.collect_shader(pixel_shader, ShaderKind::Pixel);
+                    }
+                }
+                Command::Dispatch { pipeline, .. } => {
+                    self.collect_shader(pipeline.address_lo, ShaderKind::Compute);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn collect_shader(&mut self, shader_address: u32, kind: ShaderKind) {
+        if shader_address == 0 {
+            return;
+        }
+
+        let entry = self.shaders.entry(shader_address);
+        let Entry::Vacant(item) = entry else {
+            return;
+        };
+
+        let Ok(shader_invocation) = (unsafe { ShaderInvocation::decode_from_memory(shader_address) }) else {
+            return;
+        };
+
+        item.insert(
+            Shader {
+                shader_invocation,
+                kind,
+            }
+        );
     }
 }
